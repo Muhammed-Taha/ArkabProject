039b7d83e80439daac27cbac569d175657581330
mustafa-ashraf <your_email_address@example.com>
2022-01-02 16:01:29 +0200

    Chnages

--- Fcai_SW2/Fcai_SW2/src/fcai_sw2/Admin.java
+++ Fcai_SW2/Fcai_SW2/src/fcai_sw2/Admin.java
@@ -3,81 +3,49 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fcai_sw2;
+package Arkap;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import DataBase.*;
 /**
  *
  * @author omer
  */
-public class Admin extends General_user {
+public class Admin extends User {
    static ArrayList<Driver> Pending_list = new ArrayList<Driver>();
    static ArrayList<Driver> driverList = new ArrayList<Driver>();
    static ArrayList<User> U_list = new ArrayList<User>();
-   public Admin(){
-   }
    
-   static public ArrayList All()
-  {
-        try {
-            ArrayList<Driver> al=new ArrayList<Driver>();
-            Connection co = DriverManager.getConnection("jdbc:mysql://localhost/fcai_sw2?serverTimezone=UTC", "admin","fci20190352");
-            Statement s=co.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
-            ResultSet r = s.executeQuery("SELECT * FROM `driver`");
-            while(r.next()&&r.getBoolean("state")==false)
-            {
-                Driver driver=new Driver();
-                driver.username=r.getString("username");
-                driver.mobile_number=r.getString("mobile_number");
-                driver.email=r.getString("email");
-                driver.password=r.getString("password");
-                driver.driving_license=r.getString("driving_license");
-                driver.national_id=r.getString("national_id");
-                String f =new String();
-                f=r.getString("favoriteAreas");
-                if(f!=null){
-                String [] arr;
-                arr=f.split("/");
-                for(int i=0;i<arr.length;i++){
-                   driver.favoriteAreas.add(arr[i]);
-                }
-                }
-                else{
-                    driver.favoriteAreas=null;
-                }
-                if(r.getString("rates")!=null){
-                    driver.rates=r.getString("rates");
-                }
-                else{
-                     driver.rates=null;
-                }
-                al.add(driver);
-            }
-            return al;
-        }
-        catch(SQLException e)
-        {
-            e.printStackTrace();
-        }
-        return null;
-        
-
-    }
+   static DiscountArea discount =new DiscountArea();
+   public Admin(){
+     discount.setDiscountAreas(discountControler.All());
+   }
    public  void driverVerfiy(){
-      Pending_list=this.All();
+      Pending_list=driverControler.All(false);
        for(int i=0;i<Pending_list.size();i++){
            if((Pending_list.get(i).getNational_id()!=null)&&(Pending_list.get(i).getDriving_license()!=null)){
-              Pending_list.get(i).update(Pending_list.get(i).getMobile_number());
+              driverControler.update(Pending_list.get(i).getMobileNum());
            }
        }
    }
-   public void  Suspend(Registertion_users reg){
-      reg.update(reg.mobile_number);
+   public void Suspend_Client(Client reg){
+       clientControler.update(reg.getMobileNum());
    }
+   public void  Suspend_driver(Driver reg){
+      driverControler.update(reg.getMobileNum());
+   }
+   public void getEventDetails(){
    
+   }
+    public static DiscountArea getDiscount() {
+        return discount;
+    }
+    public static void setDiscount(DiscountArea discount) {
+        Admin.discount = discount;
+    }
 }
 
